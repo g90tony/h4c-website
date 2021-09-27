74dfbd5e29e1d575e6016afedb2e3b0a7f025e42
Caleb Tony <calebmbugua@gmail.com>
2021-09-27 16:40:51 +0300

    completed fetch first page functionality

---
+++ javascript/custom/api/gallery-page.js
@@ -0,0 +1,22 @@
+$("document").ready(fetchGalleryPageImages);
+
+function fetchGalleryPageImages() {
+  const configurations = {
+    url: "http://localhost:3000/api/gallery/get/image/all/1",
+    method: "GET",
+    timeout: 0,
+  };
+
+  $.ajax(configurations).done(function (response) {
+    const { data } = response;
+
+    data.forEach((image) => {
+      const image_src = image.path;
+      $("#galleryPageGrid").append(
+        `<div class="custom-gallery-item" id='${image.originalname}'>
+          <img src="${image_src}" class='custom-gallery-image' id='${image.originalname}image'
+        </div>`
+      );
+    });
+  });
+}
